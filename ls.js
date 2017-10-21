63374c030ee3a877f1c435197817896c0fcae0f3
Fidiam-Jonathon <Fid15001@byui.edu>
2017-10-20 19:55:11 -0700

    added LS and transformation demos

--- ls.js
+++ ls.js
@@ -1,36 +1,8 @@
 let counter = 0;
 function storageAvailable(type) {
-    try {
-        var storage = window[type],
-            x = '__storage_test__';
-        storage.setItem(x, x);
-        storage.removeItem(x);
-        return true;
-    }
-    catch(e) {
-        return e instanceof DOMException && (
-            // everything except Firefox
-            e.code === 22 ||
-            // Firefox
-            e.code === 1014 ||
-            // test name field too, because code might not be present
-            // everything except Firefox
-            e.name === 'QuotaExceededError' ||
-            // Firefox
-            e.name === 'NS_ERROR_DOM_QUOTA_REACHED') &&
-            // acknowledge QuotaExceededError only if there's something already stored
-            storage.length !== 0;
-    }
-}
-
-if (storageAvailable('localStorage')) {
   let input = document.getElementById("movie").value;
-
   localStorage.setItem('movie' + counter, input);
   counter++;
-}
-else {
-  alert("LocalStorage is not available")
 }
 
 function getFromStorage(){
@@ -47,3 +19,9 @@ let showMovies = document.getElementById("movieBtn");
 showMovies.addEventListener("click", getFromStorage);
 let inputMovies = document.getElementById("button");
 inputMovies.addEventListener("click", storageAvailable);
+
+let doOver = document.getElementById("clear");
+doOver.addEventListener("click", function(){
+
+  localStorage.clear();
+});
